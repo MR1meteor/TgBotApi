35d0d1afd8c6b5ec6e51e3513c75b3829e89d1a5
Alexey Safronov <a.safronov@smartway.today>
2023-11-04 01:40:30 +0300

    Add screenshots by link

---
+++ TgBotApi/Controllers/VisualController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TgBotApi.Services.Interfaces;
+
+namespace TgBotApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisualController : ControllerBase
+    {
+        private readonly IVisualService visualService;
+
+        public VisualController(IVisualService visualService)
+        {
+            this.visualService = visualService;
+        }
+
+        [HttpGet("{link}")]
+        public async Task<IActionResult> GetByLink(string link)
+        {
+            var encodedScreenshot = await visualService.GetByLink(link);
+
+            if (encodedScreenshot != null)
+            {
+                return Ok(encodedScreenshot);
+            }
+
+            return BadRequest();
+        }
+    }
+}
