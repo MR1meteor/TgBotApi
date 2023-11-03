b74c54b0b39854264fffa2a92ca4bf5f03944fec
Alexey Safronov <a.safronov@smartway.today>
2023-11-04 02:38:50 +0300

    Add links crud

---
+++ TgBotApi/Repositories/Interfaces/ILinkRepository.cs
@@ -0,0 +1,11 @@
+﻿using TgBotApi.Models;
+
+namespace TgBotApi.Repositories.Interfaces
+{
+    public interface ILinkRepository
+    {
+        Task<LinkModel?> Add(LinkModel linkModel);
+        Task<LinkModel?> Get(int id);
+        Task<List<LinkModel>> GetAllByCredential(int credentialId);
+    }
+}
---
+++ TgBotApi/Services/LinkService.cs
@@ -0,0 +1,37 @@
+﻿using TgBotApi.Models;
+using TgBotApi.Repositories.Interfaces;
+using TgBotApi.Services.Interfaces;
+
+namespace TgBotApi.Services
+{
+    public class LinkService : ILinkService
+    {
+        private readonly ILinkRepository linkRepository;
+
+        public LinkService(ILinkRepository linkRepository)
+        {
+            this.linkRepository = linkRepository;
+        }
+
+        public async Task<LinkModel?> AddLink(LinkDto model)
+        {
+            var newLinkModel = new LinkModel
+            {
+                CredentialId = model.CredentialId,
+                Url = model.Url,
+            };
+
+            return await linkRepository.Add(newLinkModel);
+        }
+
+        public async Task<List<LinkModel>> GetByCredential(int credentialId)
+        {
+            return await linkRepository.GetAllByCredential(credentialId);
+        }
+
+        public async Task<LinkModel?> GetByid(int id)
+        {
+            return await linkRepository.Get(id);
+        }
+    }
+}
