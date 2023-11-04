5085949e56dd63d3bccd7787d619f8d073868d12
Alexey Safronov <a.safronov@smartway.today>
2023-11-04 23:13:38 +0300

    selenium google driver fix

--- TgBotApi/Services/VisualService.cs
+++ TgBotApi/Services/VisualService.cs
@@ -1,6 +1,5 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
 using TgBotApi.Services.Interfaces;
 
@@ -23,18 +22,21 @@ namespace TgBotApi.Services
             options.AddArguments("--window-size=1440,1080");
             var driver = new RemoteWebDriver(new Uri("http://selenium-hub:4444/wd/hub"), options);
 
-            var normalUrl = System.Web.HttpUtility.UrlDecode(link);
+            Screenshot? screenshot;
 
-            driver.Navigate().GoToUrl(normalUrl);
-
-            Screenshot? screenshot = null;
             try
             {
+                var normalUrl = System.Web.HttpUtility.UrlDecode(link);
+
+                driver.Navigate().GoToUrl(normalUrl);
+
                 screenshot = ((ITakesScreenshot)driver).GetScreenshot();
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                driver.Quit();
+                logger.LogError(ex.Message, ex.Data);
+                throw;
             }
 
             driver.Quit();
