d0334752e825cb77016f7988ed3df11da83e80d9
Alexey Safronov <a.safronov@smartway.today>
2023-11-05 06:43:58 +0300

    massive fix

--- TgBotApi/Repositories/Interfaces/ISshRepository.cs
+++ TgBotApi/Repositories/Interfaces/ISshRepository.cs
@@ -7,8 +7,8 @@ public interface ISshRepository
     Task<SshConnect> GetSshString(int UserId);
     Task<bool> SetSshSting(SshConnect connect);
     void AddQuery(SshQuery query);
-    Task<List<SshQuery>> GetQuery(int userId);
-    void DeleteQuery(int queryId);
+    Task<List<SshQuery>> GetQuery(int credentialsId);
+    Task DeleteQuery(int credentialsId, string queryName);
     Task InsertSQLDumps(string sql, int credentialsId); 
     Task<SshQuery> UpdateQuery(SshQuery query);
     Task<List<CredentialAndDatabase>> SelectAllConnections(int userId);
